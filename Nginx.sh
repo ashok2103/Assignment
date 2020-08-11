36cd62b6817aaaa371318aab410217613d337c79
ashok2103 <64363618+ashok2103@users.noreply.github.com>
2020-08-11 23:25:47 +0530

    Create Nginx.sh

---
+++ Nginx.sh
@@ -0,0 +1,10 @@
+#shell script
+#!/bin/bash
+#To upate the isntance
+sudo apt-get update
+#To install nginx
+sudo apt-get install -y nginx
+#Creating the index file in nginx under /var/www/html/ with Hello-world
+sudo sh -c 'echo "Hello-World" >> /var/www/html/index.html'
+#starting the nginx service
+sudo service nginx start
